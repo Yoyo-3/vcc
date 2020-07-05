3e3ad94c9261a511c92163d930e31d77de6b0971
Yoyo-3 <2321096387@qq.com>
2020-07-06 00:09:18 +0800

    2020\7\5

---
+++ 综合实验三/MFC13.3/MyDlg.cpp
@@ -0,0 +1,36 @@
+// MyDlg.cpp : 实现文件
+//
+
+#include "stdafx.h"
+#include "MFC13.3.h"
+#include "MyDlg.h"
+#include "afxdialogex.h"
+
+
+// MyDlg 对话框
+
+IMPLEMENT_DYNAMIC(MyDlg, CDialogEx)
+
+MyDlg::MyDlg(CWnd* pParent /*=NULL*/)
+	: CDialogEx(IDD_DIALOG1, pParent)
+	, i(0)
+{
+
+}
+
+MyDlg::~MyDlg()
+{
+}
+
+void MyDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+	DDX_Text(pDX, IDC_EDIT1, i);
+}
+
+
+BEGIN_MESSAGE_MAP(MyDlg, CDialogEx)
+END_MESSAGE_MAP()
+
+
+// MyDlg 消息处理程序
