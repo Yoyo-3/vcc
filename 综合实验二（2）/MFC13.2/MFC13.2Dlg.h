53f9ef6dae96630fb55838002b6b40c57beead40
Yoyo-3 <2321096387@qq.com>
2020-07-06 00:07:36 +0800

    2020\7\5

---
+++ 综合实验二（2）/MFC13.2/MFC13.2Dlg.h
@@ -0,0 +1,35 @@
+
+// MFC13.2Dlg.h : 头文件
+//
+
+#pragma once
+
+
+// CMFC132Dlg 对话框
+class CMFC132Dlg : public CDialogEx
+{
+// 构造
+public:
+	CMFC132Dlg(CWnd* pParent = NULL);	// 标准构造函数
+	CString s;
+
+// 对话框数据
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_MFC132_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 支持
+
+
+// 实现
+protected:
+	HICON m_hIcon;
+
+	// 生成的消息映射函数
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+};
